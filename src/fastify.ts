08180d6e1d4d2c85fca9f1498cbfa8be6c7412a9
Luke Bayliss <hello@lukebayliss.com>
2024-04-24 21:24:51 +1000

    asdasd

--- src/fastify.ts
+++ src/fastify.ts
@@ -10,11 +10,24 @@ const ServerLive = Layer.scopedDiscard(
   Effect.gen(function* (_) {
     const port = 3000;
     const app = yield* _(ElysiaService);
-    yield* _(Effect.acquireRelease(
-      Effect.sync(() => {
-        app.listen(port)
-      }),
-      (server) => Effect.sync()
-    ));
+    yield* _(
+      Effect.acquireRelease(
+        Effect.sync(() =>
+          app.listen(port, () =>
+            console.log(`Example app listening on port ${port}`)
+          )
+        ),
+        (server) => Effect.sync(() => server.stop())
+      )
+    );
   })
 );
+
+const ElysiaLive = Layer.sync(ElysiaService, () => new Elysia());
+
+const AppLive = ServerLive.pipe(
+  Layer.provide(ServerLive),
+  Layer.provide(ElysiaLive)
+);
+
+Effect.runFork(Layer.launch(AppLive));
